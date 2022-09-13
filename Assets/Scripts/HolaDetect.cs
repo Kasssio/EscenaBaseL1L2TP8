a295ab9355640829e7ca0716d44caeb5671dc8d7
Lucas Estrada <101399158+Kasssio@users.noreply.github.com>
2022-09-13 09:57:40 -0300

    Interacción hecha

--- Assets/Scripts/HolaDetect.cs
+++ Assets/Scripts/HolaDetect.cs
@@ -7,26 +7,52 @@ public class HolaDetect : MonoBehaviour
 {
     [SerializeField] GameObject UIElements;
     [SerializeField] TextMeshProUGUI dialogueTxt;
-    [SerializeField] string[] NPCDialogue;
+    [SerializeField] NPCData Datita;
+    [SerializeField] AudioClip StickyKeysSound;
+
+    GameObject[] computadoras;
+
+    public bool HasRepairedComputer = false;
+    private bool ComputerHasBeenSelected = false;
+
+    private string[] NPCDialogue = null; // solo hay q cambiar el dialogo cuando repare la compu 
+    
+    private int IntroMsgIndex = 0;
+    
+   
     // Start is called before the first frame update
     void Start()
     {
         UIElements.SetActive(false);
+        computadoras = GameObject.FindGameObjectsWithTag("Monitor");
+
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AvanzarDialogo();
     }
+
+
     void OnTriggerEnter(Collider col)
-    {
-        if (col.gameObject.CompareTag("JeroPrisma"))
+    { //yo cavernicola, yo chocar contra pared ??? por lo de 
+
+
+        if(col.gameObject.GetComponent<NPCDialogue>() && !HasRepairedComputer)
         {
-            UIElements.SetActive(true);
             NPCDialogue = col.gameObject.GetComponent<NPCDialogue>().data.dialogueLines;
+            UIElements.SetActive(true);
+
+            dialogueTxt.text = NPCDialogue[IntroMsgIndex];
         }
 
+
+        else if(col.gameObject.GetComponent<NPCDialogue>() && HasRepairedComputer)
+        {
+            //cambiar al dialogo q ya la reparo. acordate de cambiar el booleano
+        }
     }
 
     void OnTriggerExit(Collider col)
@@ -40,10 +66,39 @@ public class HolaDetect : MonoBehaviour
 
     public void AvanzarDialogo()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && dialogueTxt.IsActive() && !HasRepairedComputer)
         {
-            dialogueTxt.text = NPCDialogue[1];
+            if(IntroMsgIndex < 2 && !ComputerHasBeenSelected)               
+            {
+                dialogueTxt.text = NPCDialogue[++IntroMsgIndex];
+            }
+
+             if(IntroMsgIndex >= 2)
+             {
+                
+                ComputerHasBeenSelected = MonitorRandom(); //si
+             }
+           
         }
-            
+    }
+
+
+    bool MonitorRandom()
+    {
+
+        GameObject CompSeleccionada = computadoras[Random.Range(0, computadoras.Length)];
+
+        CompSeleccionada.gameObject.AddComponent<AudioSource>(); 
+
+        CompSeleccionada.GetComponent<AudioSource>().clip = StickyKeysSound;
+        CompSeleccionada.GetComponent<AudioSource>().loop = true;
+        CompSeleccionada.GetComponent<AudioSource>().maxDistance = 3;
+        CompSeleccionada.GetComponent<AudioSource>().minDistance = 1;
+        CompSeleccionada.GetComponent<AudioSource>().rolloffMode = AudioRolloffMode.Linear;
+        CompSeleccionada.GetComponent<AudioSource>().spatialBlend = 1;
+        CompSeleccionada.GetComponent<AudioSource>().Play();
+
+        return true;
     }
 }
+//veni a ds que quiero entender, no que me lo hagas :( xd
--- Assets/Scripts/NPCDialogue.cs
+++ Assets/Scripts/NPCDialogue.cs
@@ -5,15 +5,5 @@ using UnityEngine;
 public class NPCDialogue : MonoBehaviour
 {
     public NPCData data;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
